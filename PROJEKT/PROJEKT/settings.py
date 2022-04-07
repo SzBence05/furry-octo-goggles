fb19628008ba53c93900485e1dfcd3359084f379
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-04-07 21:04:18 +0200

    Heroku dune

--- PROJEKT/PROJEKT/settings.py
+++ PROJEKT/PROJEKT/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
