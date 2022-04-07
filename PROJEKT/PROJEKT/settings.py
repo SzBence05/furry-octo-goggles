218e82ba0b8e3457ae7a8614f6374f23286bc337
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-04-07 21:08:50 +0200

    .

--- PROJEKT/PROJEKT/settings.py
+++ PROJEKT/PROJEKT/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = 'staticfles/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
